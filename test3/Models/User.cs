9715c0f1362ea5b1074578c2b63ffbe3fa3ed148
Цылиакус Максм <tsyliakusmaxim@gmail.com>
2019-11-30 21:04:47 +0300

    - прототип

---
+++ test3/Models/User.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.DataProtection.KeyManagement;
+using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+
+namespace test3.Models
+{
+    public class User:IdentityUser
+    {
+        //[Key]
+        //public IKey Id { get; set; }
+        //public string UserName { get; set; }
+        //public string Email { get; set; }
+        //public string PhoneNumber { get; set; }
+    }
+}
